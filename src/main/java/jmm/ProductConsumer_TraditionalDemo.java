6aa28662eddd0a084f85f791e4beb5bed8d346d1
wen <1594957790@qq.com>
2021-05-13 23:55:13 +0800

    多个condition控制

--- src/main/java/jmm/ProductConsumer_TraditionalDemo.java
+++ src/main/java/jmm/ProductConsumer_TraditionalDemo.java
@@ -5,11 +5,17 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 传统的生产者消费者模式
+ * 传统的 线程通讯  生产者消费者模式
+ *
+ * 多线程的判断必须使用 while ==》不然会出现
  */
 public class ProductConsumer_TraditionalDemo {
 
 
+    /**
+     * 要求：两个线程，一个加一，一个减一，执行5轮
+     * @param args
+     */
     public static void main(String[] args) {
 
         MyShareData shareData = new MyShareData();
@@ -34,6 +40,28 @@ public class ProductConsumer_TraditionalDemo {
                 }
             }
         },"BB").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                try {
+                    shareData.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        },"CC").start();
+
+        new Thread(()->{
+
+            for (int i = 0; i < 5; i++) {
+                try {
+                    shareData.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"DD").start();
     }
 
 }
@@ -56,7 +84,7 @@ class MyShareData{
             }
             num ++ ;
             System.out.println(Thread.currentThread().getName() + "\t" + num + "\t" + lock.hashCode());
-            condition.signal();
+            condition.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -74,7 +102,7 @@ class MyShareData{
             }
             num -- ;
             System.out.println(Thread.currentThread().getName() + "\t" + num + "\t" + lock.hashCode());
-            condition.signal();
+            condition.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
---
+++ src/main/java/jmm/ProductConsumer_TraditionalDemo2.java
@@ -0,0 +1,119 @@
+package jmm;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ProductConsumer_TraditionalDemo2 {
+
+    /**
+     * 需求：
+     * AA打印5次，BB打印10次，CC打印15次
+     * 来5轮
+     */
+
+    public static void main(String[] args) {
+
+        ShareResource resource = new ShareResource();
+
+        new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                resource.print5();
+            }
+
+        },"AA").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                resource.print10();
+            }
+
+        },"BB").start();
+
+        new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                resource.print15();
+            }
+
+        },"CC").start();
+
+    }
+}
+
+class ShareResource{
+
+    private int num = 1;
+    Lock lock = new ReentrantLock();
+    Condition c1 = lock.newCondition();
+    Condition c2 = lock.newCondition();
+    Condition c3 = lock.newCondition();
+
+    public void print5() {
+        lock.lock();
+        try {
+            // 判断 条件
+            while (num != 1){
+                c1.await();
+            }
+
+            // 干活
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" +i);
+            }
+
+            //通知
+            num = 2;
+            c2.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10() {
+        lock.lock();
+        try {
+            // 判断 条件
+            while (num != 2){
+                c2.await();
+            }
+
+            // 干活
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" +i);
+            }
+
+            //通知
+            num = 3;
+            c3.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print15() {
+        lock.lock();
+        try {
+            // 判断 条件
+            while (num != 3){
+                c3.await();
+            }
+
+            // 干活
+            for (int i = 0; i < 15; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" +i);
+            }
+
+            //通知
+            num = 1;
+            c1.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
