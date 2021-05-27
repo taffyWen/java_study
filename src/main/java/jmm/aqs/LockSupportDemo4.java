72cd2358e8195eabde19967ea3e2a17cbf172932
wen <1594957790@qq.com>
2021-05-27 23:42:42 +0800

    LockSupport

---
+++ src/main/java/jmm/aqs/LockSupportDemo4.java
@@ -0,0 +1,43 @@
+package jmm.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author wen
+ * @create 2021/5/25 23:27
+ */
+public class LockSupportDemo4 {
+
+    public static void main(String[] args) {
+
+        Lock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+        Object obj = new Object();
+        Object obj2 = new Object();
+
+        Thread a = new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "\t ----come in");
+
+            LockSupport.park();
+
+            System.out.println(Thread.currentThread().getName() + "\t ----被唤醒");
+        },"AA");
+        a.start();
+
+        new Thread(()->{
+
+            LockSupport.unpark(a);
+            System.out.println(Thread.currentThread().getName() + "\t ----通知");
+
+        },"BB").start();
+    }
+}
